c2b4d26a9c115c7f75736d6aeb6dd5e6271b08aa
ParrishJ <jarparrish@gmail.com>
2019-06-10 13:21:31 -0700

    more structured put in place, configured migrations

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const authorizationRouter = require('../authorization/auth-router');
+const userRouter = require('../users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/users', userRouter);
+server.use('/api/auth', authorizationRouter);
+
+module.exports = server;
